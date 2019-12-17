65c067a9b344a634cb5504911f90fdfad11eda29
Gabriel Oliveira <gabrieloliveira851@hotmail.com>
2019-12-17 21:23:25 -0200

    Lendo arquivo
    leitura de arquivo txt ou csv com streamReader

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader streamReader = new StreamReader(@"\\10.75.0.90\publico$\ArquivoCsv\ListaDeRamais.txt"))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    string id = "";
+                    string nome = "";
+                    string ramal = "";
+
+                    String linha = streamReader.ReadLine();
+
+                    var array = linha.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    id = array[0];
+                    nome = array[1];
+                    ramal = array[2];
+
+                    Console.WriteLine("ID: "+id + " Nome: " + nome + ", Ramal: " + ramal);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
